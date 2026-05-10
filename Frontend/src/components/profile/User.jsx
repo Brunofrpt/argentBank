a662f7535e584b9221ba655fe35c4c4957b21edb
Bruno <alvalade@live.fr>
2026-05-10 10:19:21 +0200

    mise a jour des propositions de routes dans la fichier swagger yaml

--- Frontend/src/components/profile/User.jsx
+++ Frontend/src/components/profile/User.jsx
@@ -8,14 +8,14 @@ function User() {
   return (
     <div className="header">
       {isEditing ? (
-        <EditUserName setIsEditing={setIsEditing}/>
-  ) : (
-    <>
-      <h1>Welcome back<br /> {user ? `${user.firstName} ${user.lastName} !` : ""} </h1>
-      <button onClick={() => setIsEditing(true)} className="edit-button">Edit Name</button>
-    </>
-  )}
-  </div>
+        <EditUserName setIsEditing={setIsEditing} />
+      ) : (
+        <>
+          <h1>Welcome back<br /> {user ? `${user.firstName} ${user.lastName} !` : ""} </h1>
+          <button onClick={() => setIsEditing(true)} className="edit-button">Edit Name</button>
+        </>
+      )}
+    </div>
   )
 }
 
